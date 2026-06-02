8d2f2afa6c32b17cef1820deda23a37dbc2f54a9
macaul_b <budmacaulay97@outlook.com>
2026-06-02 14:56:44 +0200

    add simple optimade IO

--- lib/core/io/index.ts
+++ lib/core/io/index.ts
@@ -3,3 +3,6 @@ export * from "./poscar";
 export * from "./structuredata";
 export * from "./xsf";
 export * from "./xyz";
+export * from "./optimade";
+
+export * from "./json";
---
+++ lib/core/io/optimade.ts
@@ -0,0 +1,60 @@
+import { createLattice } from "../lattice";
+
+import { fractional, cartesian } from "../site";
+import { Structure } from "../structure";
+
+export function fromOptimade(data: any): Structure {
+  const lattice = createLattice([
+    ...data.attributes.lattice_vectors[0],
+    ...data.attributes.lattice_vectors[1],
+    ...data.attributes.lattice_vectors[2],
+  ]);
+
+  const positions = data.attributes.cartesian_site_positions;
+  const species = data.attributes.species_at_sites;
+
+  const sites = positions.map((position: number[], i: number) => ({
+    species: {
+      symbol: species[i],
+    },
+
+    frac: fractional(lattice, new Float64Array(position)),
+  }));
+
+  return {
+    lattice,
+    sites,
+  };
+}
+
+export function toOptimade(structure: Structure) {
+  const m = structure.lattice.basis.data;
+
+  const species_at_sites = structure.sites.map((site) => site.species.symbol);
+
+  const elements = [...new Set(species_at_sites)].sort();
+
+  return {
+    attributes: {
+      lattice_vectors: [
+        [m[0], m[1], m[2]],
+        [m[3], m[4], m[5]],
+        [m[6], m[7], m[8]],
+      ],
+
+      species_at_sites,
+
+      cartesian_site_positions: structure.sites.map((site) =>
+        Array.from(cartesian(structure.lattice, site)),
+      ),
+
+      dimension_types: [1, 1, 1],
+
+      nsites: structure.sites.length,
+
+      nelements: elements.length,
+
+      elements,
+    },
+  };
+}
